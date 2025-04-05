44408e1db271189547d9e518d72b9dfaa7b4413e
hooiv <125276621+hooiv@users.noreply.github.com>
2025-04-05 04:52:13 +0000

    Apr 5, 2025, 12:52 AM

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,11 +1,7 @@
 // frontend/postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // Updated to use the new separate package
     autoprefixer: {},
-    // If you installed @tailwindcss/postcss explicitly,
-    // you might not even need it listed here if using a recent
-    // version of tailwindcss itself, but including it doesn't hurt.
-    // '@tailwindcss/postcss': {}, // Usually implicitly handled by 'tailwindcss' above now
   },
 }
