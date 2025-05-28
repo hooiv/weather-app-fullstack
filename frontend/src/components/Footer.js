cdf10209515bef71cfe704431306512999866997
ADITYA CHAUHAN <125276621+hooiv@users.noreply.github.com>
2025-05-28 16:20:33 +0000

    Changes to be committed: 	modified:   frontend/src/components/Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,19 +4,6 @@ const Footer = ({ yourName }) => {
     return (
         <footer className="mt-8 pt-4 border-t border-gray-300 text-xs text-gray-500">
             <p className="mb-2">Developed by: {"Aditya Chauhan"}</p>
-            <p className="font-semibold">About Product Manager Accelerator (PMA):</p>
-            <p className="mb-2">
-            From entry-level to VP of Product, we support PM professionals through every stage of their careers.
-
-</p>
-            <a
-                href="https://www.linkedin.com/school/pmaccelerator/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 hover:underline"
-            >
-                Visit PMA on LinkedIn
-            </a>
         </footer>
     );
 };
